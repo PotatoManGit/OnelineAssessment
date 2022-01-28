2757e8d5983f8e988f2b0cbfd72058162b469739
PotatoMan <potatoman189@163.com>
2022-01-28 10:58:08 +0800

    project created

---
+++ app/Http/Controllers/Index.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+/**
+ * Class Index
+ * @package App\Http\Controllers
+ * 首页控制器
+ */
+
+class Index extends Controller
+{
+    public function index()
+    {
+//        // 获取cookie 验证存在性
+//        if(empty($_COOKIE['tokenId']) || empty($_COOKIE['token']))
+//        {
+//            $signInStatus = 0;
+//        }
+//        else
+//        {
+//            // 解密
+//            $db = new TT_user();
+//            $uid = Crypt::decryptString($_COOKIE['tokenId']);
+//            $password = Crypt::decryptString($_COOKIE['token']);
+//            $truePassword = $db->CheckCookie($uid);
+//            if($password == $truePassword)
+//            {
+//                $signInStatus = 1;
+//            }
+//            else
+//            {
+//                $signInStatus = 0;
+//            }
+//        }
+
+        return view('index');
+//        return redirect('user');
+    }
+}
